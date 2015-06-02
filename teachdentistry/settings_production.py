4ade13df811e6afbe910e0861dddd0ab5d1348e5
Susan Dreher <sdreher@columbia.edu>
2015-06-02 13:40:31 -0400

    S3 static resources

--- teachdentistry/settings_production.py
+++ teachdentistry/settings_production.py
@@ -1,16 +1,6 @@
 # flake8: noqa
 from settings_shared import *
 
-TEMPLATE_DIRS = (
-    "/var/www/teachdentistry/teachdentistry/teachdentistry/templates",
-)
-
-MEDIA_ROOT = '/var/www/teachdentistry/uploads/'
-# put any static media here to override app served static media
-STATICMEDIA_MOUNTS = (
-    ('/sitemedia', '/var/www/teachdentistry/teachdentistry/sitemedia'),
-)
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -22,12 +12,28 @@ DATABASES = {
     }
 }
 
-COMPRESS_ROOT = "/var/www/teachdentistry/teachdentistry/media/"
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 if 'migrate' not in sys.argv:
     INSTALLED_APPS.append('raven.contrib.django.raven_compat')
+
+# AWS Settings for staging
+AWS_STORAGE_BUCKET_NAME = "ccnmtl-teachdentistry-static-prod"
+AWS_PRELOAD_METADATA = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+S3_URL = 'https://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+# static data, e.g. css, js, etc.
+STATICFILES_STORAGE = 'cacheds3storage.CompressorS3BotoStorage'
+STATIC_URL = 'https://%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
+COMPRESS_ENABLED = True
+COMPRESS_OFFLINE = True
+COMPRESS_ROOT = STATIC_ROOT
+COMPRESS_URL = STATIC_URL
+COMPRESS_STORAGE = 'cacheds3storage.CompressorS3BotoStorage'
+# uploaded images
+MEDIA_URL = 'https://%s.s3.amazonaws.com/uploads/' % AWS_STORAGE_BUCKET_NAME
+
 
 try:
     from local_settings import *

2d7113ad1245590b90d5b9d85ba138c6054fb0c2
Anders Pearson <anders@columbia.edu>
2015-12-16 17:11:51 +0000

    convert to ccnmtlsettings

--- teachdentistry/settings_shared.py
+++ teachdentistry/settings_shared.py
@@ -1,112 +1,32 @@
 # Django settings for teachdentistry project.
 import os.path
-import sys
+from ccnmtlsettings.shared import common
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
-
-ADMINS = ()
-
-MANAGERS = ADMINS
+project = 'teachdentistry'
+base = os.path.dirname(__file__)
+locals().update(common(project=project, base=base))
 
 AUTH_PROFILE_MODULE = "main.UserProfile"
 ACCOUNT_ACTIVATION_DAYS = 7
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'teachdentistry',
-        'HOST': '',
-        'PORT': 5432,
-        'USER': '',
-        'PASSWORD': '',
-        'ATOMIC_REQUESTS': True,
-    }
-}
-
-if 'test' in sys.argv or 'jenkins' in sys.argv:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': ':memory:',
-            'HOST': '',
-            'PORT': '',
-            'USER': '',
-            'PASSWORD': '',
-            'ATOMIC_REQUESTS': True,
-        }
-    }
-
-TEST_RUNNER = 'django.test.runner.DiscoverRunner'
-
-JENKINS_TASKS = (
-    'django_jenkins.tasks.run_pep8',
-    'django_jenkins.tasks.run_pyflakes',
-)
 PROJECT_APPS = [
     'teachdentistry.main',
 ]
 
-ALLOWED_HOSTS = ['localhost', '.ccnmtl.columbia.edu', '.teachdentistry.org']
+ALLOWED_HOSTS += ['.teachdentistry.org']  # noqa
 
 USE_TZ = True
-TIME_ZONE = 'America/New_York'
-LANGUAGE_CODE = 'en-us'
-SITE_ID = 1
-USE_I18N = False
-SECRET_KEY = ')ng#)ef_u@_^zvvu@dxm7ql-yb^_!a6%v3v^j3b(mp+)l+5%@h'
 
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-)
-
-TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.contrib.auth.context_processors.auth',
-    'django.template.context_processors.debug',
-    'django.template.context_processors.request',
-    'django.template.context_processors.static',
-    'django.template.context_processors.media',
-    'stagingcontext.staging_processor',
-    'djangowind.context.context_processor',
+TEMPLATE_CONTEXT_PROCESSORS += [  # noqa
     'teachdentistry.main.views.context_processor'
-)
+]
 
-MIDDLEWARE_CLASSES = (
-    'django_statsd.middleware.GraphiteRequestTimingMiddleware',
-    'django_statsd.middleware.GraphiteMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-    'waffle.middleware.WaffleMiddleware',
-)
-
-ROOT_URLCONF = 'teachdentistry.urls'
-
-TEMPLATE_DIRS = (os.path.join(os.path.dirname(__file__), "templates"),)
-
-INSTALLED_APPS = [
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.flatpages',
-    'django.contrib.staticfiles',
+INSTALLED_APPS += [  # noqa
     'sorl.thumbnail',
-    'django.contrib.admin',
     'tagging',
     'typogrify',
-    'compressor',
-    'django_statsd',
     'bootstrapform',
     'lettuce.django',
-    'debug_toolbar',
-    'waffle',
-    'django_jenkins',
-    'smoketest',
     'django_extensions',
     'teachdentistry.main',
     'pagetree',
@@ -114,10 +34,7 @@ INSTALLED_APPS = [
     'quizblock',
     'treebeard',
     'registration',
-    'django_markwhat',
     'localflavor',
-    'gunicorn',
-    'storages'
 ]
 
 PAGEBLOCKS = [
@@ -134,63 +51,4 @@ LETTUCE_APPS = (
     'teachdentistry.main',
 )
 
-INTERNAL_IPS = ('127.0.0.1', )
-DEBUG_TOOLBAR_PANELS = (
-    'debug_toolbar.panels.version.VersionDebugPanel',
-    'debug_toolbar.panels.timer.TimerDebugPanel',
-    'debug_toolbar.panels.headers.HeaderDebugPanel',
-    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
-    'debug_toolbar.panels.template.TemplateDebugPanel',
-    'debug_toolbar.panels.sql.SQLDebugPanel',
-    'debug_toolbar.panels.signals.SignalDebugPanel',
-)
-
-
-STATSD_CLIENT = 'statsd.client'
-STATSD_PREFIX = 'teachdentistry'
-STATSD_HOST = '127.0.0.1'
-STATSD_PORT = 8125
-
 THUMBNAIL_SUBDIR = "thumbs"
-EMAIL_SUBJECT_PREFIX = "[teachdentistry] "
-EMAIL_HOST = 'localhost'
-SERVER_EMAIL = "teachdentistry@ccnmtl.columbia.edu"
-DEFAULT_FROM_EMAIL = SERVER_EMAIL
-
-MEDIA_URL = "/uploads/"
-MEDIA_ROOT = 'uploads'
-STATIC_URL = "/media/"
-STATIC_ROOT = "/tmp/teachdentistry/static"
-STATICFILES_DIRS = ('media/',)
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'compressor.finders.CompressorFinder',
-)
-COMPRESS_URL = "/media/"
-COMPRESS_ROOT = "media/"
-AWS_QUERYSTRING_AUTH = False
-
-
-# WIND settings
-
-AUTHENTICATION_BACKENDS = ('djangowind.auth.SAMLAuthBackend',
-                           'django.contrib.auth.backends.ModelBackend', )
-CAS_BASE = "https://cas.columbia.edu/"
-WIND_PROFILE_HANDLERS = ['djangowind.auth.CDAPProfileHandler']
-WIND_AFFIL_HANDLERS = ['djangowind.auth.AffilGroupMapper',
-                       'djangowind.auth.StaffMapper',
-                       'djangowind.auth.SuperuserMapper']
-WIND_STAFF_MAPPER_GROUPS = ['tlc.cunix.local:columbia.edu']
-WIND_SUPERUSER_MAPPER_GROUPS = ['anp8', 'jb2410', 'zm4', 'egr2107',
-                                'sld2131', 'amm8', 'mar227']
-
-SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
-SESSION_COOKIE_HTTPONLY = True
-
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTOCOL', 'https')
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-}
